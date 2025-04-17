83f344c71d90cc884033f15cea0a4d2a1b2d5f0d
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-17 15:04:45 +0000

    Volume of a Triangular Pyramid

--- results.php
+++ results.php
@@ -1,11 +1,12 @@
 <?php
-	// get the base and height from the textfields
+	// get the bases and height from the textfields
 	$baseA = $_POST['base1'];
 	$baseB = $_POST['base2'];
 	$height = $_POST['height'];
 
-	// calculate the area
+	// calculate the volume
 	$volume = (1/3) * $baseA * $baseB * $height;
+	$volume = round($volume,2);
 ?>
 	<h3>Results:</h3>
-	The area of the trapezoid is <?php echo "$volume" ?>cm<sup>3</sup>.
+	The volume of the Triangular Pyramid is <?php echo "$volume" ?>cm<sup>3</sup>.
